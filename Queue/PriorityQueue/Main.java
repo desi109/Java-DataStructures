0796c250aa84e6fa8aa011cd25abe35c4c80083d
Desislava Milusheva <40354264+desi109@users.noreply.github.com>
2019-11-03 20:35:38 +0200

    Create Main.java

---
+++ Queue/PriorityQueue/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) throws IOException
+    {
+        PriorityQ thePQ = new PriorityQ(5);
+        thePQ.insert(30);
+        thePQ.insert(50);
+        thePQ.insert(10);
+        thePQ.insert(40);
+        thePQ.insert(20);
+        while( !thePQ.isEmpty() )
+        {
+            long item = thePQ.remove();
+            System.out.print(item + " "); // 10, 20, 30, 40, 50
+        } // end while
+        System.out.println("");
+    } // end main()
+//-------------------------------------------------------------
+} 
